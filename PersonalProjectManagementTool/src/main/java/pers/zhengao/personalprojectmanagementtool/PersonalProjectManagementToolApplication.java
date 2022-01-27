668bd8a827323f13464dcc3dc889f0396751c4c0
Kate0516 <gaozhen0516@gmail.com>
2022-01-26 22:24:14 -0600

    frist commit

---
+++ PersonalProjectManagementTool/src/main/java/pers/zhengao/personalprojectmanagementtool/PersonalProjectManagementToolApplication.java
@@ -0,0 +1,13 @@
+package pers.zhengao.personalprojectmanagementtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProjectManagementToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalProjectManagementToolApplication.class, args);
+    }
+
+}
