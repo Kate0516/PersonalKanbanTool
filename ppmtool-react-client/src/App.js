27b5befc9d84d2c578e232e71bd10d56a092939c
Kate0516 <gaozhen0516@gmail.com>
2022-02-04 22:09:50 -0600

    17

---
+++ ppmtool-react-client/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css"
+import {BrowserRouter , Route, Routes} from "react-router-dom"
+import AddProject from './components/Project/AddProject';
+
+function App() {
+  return (
+      <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route exact path="/addProject" element={<AddProject />} />
+        </Routes>
+      </div>
+      </BrowserRouter>
+  );
+}
+
+export default App;
